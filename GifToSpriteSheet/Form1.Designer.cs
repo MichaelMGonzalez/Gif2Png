486be0e67e6378da887f6d8710f819186a865df6
MichaelMGonzalez <mmg005@ucsd.edu>
2016-01-23 23:46:39 -0800

    init

---
+++ GifToSpriteSheet/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace GifToSpriteSheet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(197, 226);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(75, 23);
+            this.convertButton.TabIndex = 0;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.convertButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button convertButton;
+    }
+}
+
---
+++ GifToSpriteSheet/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+
+namespace GifToSpriteSheet
+{
+    public partial class Form1 : Form
+    {
+        Stream sourceStream;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GifBitmapDecoder decoder = openFileDialog();
+            if (decoder == null)
+                return;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    int i = 0;
+                    foreach (BitmapFrame f in decoder.Frames)
+                    {
+                        Console.WriteLine("Entering foreach loop");
+                        //                        FileStream stream = File.Create(saveFileDialog1.FileName + i + ".png");
+                        Stream stream = new FileStream(saveFileDialog1.FileName + i + ".png", FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
+                        Console.WriteLine("Opening stream: " + stream);
+                        PngBitmapEncoder encoder = new PngBitmapEncoder();
+                        Console.WriteLine("Creating encoder");
+                        encoder.Frames.Add(f);
+                        Console.WriteLine("Added frame");
+                        Console.WriteLine("Saving stream");
+                        encoder.Save(stream);
+                        stream.Close();
+                        i++;
+                    }
+                    sourceStream.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+        private GifBitmapDecoder openFileDialog()
+        {
+            sourceStream = null;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    Console.WriteLine(openFileDialog1.FileName);
+                    if ((sourceStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        GifBitmapDecoder decoder = new GifBitmapDecoder(sourceStream, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+                        return decoder;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+            return null;
+        }
+    }
+}
